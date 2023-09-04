2867a96e6c3ccce1336e23b476a0554149a0beaf
Rony Ken <RM551549@fiap.com.br>
2023-09-04 09:57:05 -0300

    Utlização de Props, items do arquivo pai p/ filho

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import MeuComponente from "./components/Meucomponente";
 import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Footer from "./components/footer";
+import viteLogo from "./assets/vite.svg";
 
 export default function App(){
+
+  let viteLogoAlt = "vite-Logo"
+
   return(
     <>
       <h1>APP - Componente principal!</h1>
@@ -13,11 +16,11 @@ export default function App(){
         <Cabecalho/>
 
         {/* Iniciando a área do conteúdo */}
-        <Conteudo/>
+        <Conteudo viteLogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>
 
         {/* Iniciando a área do footer */}
         <Footer/>
       </div>
     </>
   );
-};
+}
--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,11 +1,17 @@
-export default function Conteudo(){
+import reactLogo from "../assets/react.svg"
+
+export default function Conteudo(props){
+
+    let reactLogoAlt = "React-Logo"
+
     return(
         <section>
         <div>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga nulla ad corporis exercitationem molestias cumque quae, explicabo hic quo corrupti ea repellendus temporibus expedita id? Porro quasi labore deleniti facere?</p>
           <p>Fuga asperiores sint itaque officiis libero, nostrum quaerat in nesciunt laborum aliquid, accusamus quia vitae obcaecati architecto ea modi inventore, exercitationem nam illum similique ut. Excepturi reiciendis maiores earum repellat.</p>
           <p>Obcaecati provident, pariatur adipisci perspiciatis minima magnam sequi esse numquam consequatur natus eum deleniti deserunt voluptatum atque velit soluta aliquid ea rerum veniam voluptate. Natus explicabo consequatur eius at rerum?</p>
-          <img src="./assets/react.svg" alt="Logo React" />
+          <img src={reactLogo} alt={reactLogoAlt} />
+          <img src={props.viteLogoProps} alt={props.viteLogoAltProps} />
         </div>
         </section>  
     )
